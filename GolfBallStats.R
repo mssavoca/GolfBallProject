be35614b324d28c69d08b0c96b6dc55d84864cd0
Matthew Savoca <msavoca13@gmail.com>
2018-01-03 15:14:01 -0800

    creating stats file

---
+++ GolfBallStats.R
@@ -0,0 +1,10 @@
+##########
+# Script for statistical analyses on golf ball project
+##########
+
+#load packages
+library(ggplot2); theme_set(theme_bw())
+library(tidyr)
+library(scales)
+library(ggExtra)
+library(dplyr)
